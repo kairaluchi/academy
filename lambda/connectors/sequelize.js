55e542f74bbd7bbb8db648d1abc180ebbda0b560
Alexander Obi <alexebube@gmail.com>
2020-12-24 00:17:14 -0500

    console.logs

--- lambda/connectors/sequelize.js
+++ lambda/connectors/sequelize.js
@@ -3,6 +3,7 @@ const pg = require('pg')
 
 module.exports = (config) => {
   config.dialectModule = pg
+  console.log('CONFIG', JSON.stringify(config, null, 2))
   const sequelize = new Sequelize(config.database, config.username, config.password, config)
   return {
     sequelize,
