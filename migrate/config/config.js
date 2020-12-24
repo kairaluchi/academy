f2479476b53be2ce42be7179f98691b3cca7e5b3
Alexander Obi <alexebube@gmail.com>
2020-12-23 19:55:51 -0500

    add port

--- migrate/config/config.js
+++ migrate/config/config.js
@@ -11,6 +11,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: process.env.DB_PORT
   }
 }
