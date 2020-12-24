5ec3bb814eee79cad098c7bf5e3c455f6dcfa7b8
Alexander Obi <alexebube@gmail.com>
2020-12-23 20:40:52 -0500

    build fix

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,6 +1,5 @@
 const nodeExternals = require('webpack-node-externals')
 const DotEnv = require('dotenv-webpack')
-// const webpack = require('webpack')
 
 module.exports = {
   externals: [nodeExternals()],
